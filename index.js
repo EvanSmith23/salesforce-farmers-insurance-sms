0938b8abf90da67cc70158f18be8edb10ba2776c
Louis Sheid <lxs3.dev@gmail.com>
2022-06-10 10:38:22 -0700

    add basic messaging functionality with conversation API

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 8080
+require('dotenv').config();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+app.use(bodyParser.json());
+app.use(cors());
+
+const { createConversation, addParticipantToConversation, sendMessageToParticipant, listAllMessagesWithParticpant } = require("./middleware/conversations");
 
 app.get('/', (req, res) => {
   res.send('Arrow Plumbing Messaging API working!')
 })
+
+// twilioNumber, participantNumber
+app.post('/create-conversation', createConversation, addParticipantToConversation, (req,res) => {
+  const { conversationSid } = res.locals;
+  res.end(conversationSid)
+});
+
+// converstaionId
+app.post('/send-message', sendMessageToParticipant, (req,res) => {
+  console.log("test")
+  res.end("message has been sent");
+});
+
+app.post('/list-messages', listAllMessagesWithParticpant, (req, res) => {
+  res.end("test");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ middleware/conversations.js
@@ -0,0 +1,62 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+const createConversation = (req, res, next) => {
+    console.log("CREATE CONVERSATIOn")
+    client.conversations.conversations
+        .create({friendlyName: 'My First Conversation'})
+        .then(conversation => {
+            console.log(conversation.sid);
+            res.locals.conversationSid = conversation.sid;
+            next()
+        })
+}
+
+const addParticipantToConversation = (req, res, next) => {
+    const { conversationSid } = res.locals;
+    console.log(req.body)
+    const { twilioNumber, participantNumber } = req.body;
+    console.log("IN ADD PARTICIPANTS")
+    client.conversations.conversations(conversationSid)
+        .participants
+        .create({
+        'messagingBinding.address': participantNumber,
+        'messagingBinding.proxyAddress': twilioNumber
+        })
+        .then(participant => {
+            console.log("INSIDE INSIDe", participant)
+            console.log(participant.sid)
+            res.locals.participantSid = participant.sid;
+            next()
+        }).catch(err => console.log("ERR", err))
+}
+// MBc6c91ee2369d4e5081e81457feeb31bc
+const sendMessageToParticipant = (req, res, next) => {
+    const { conversationSid, messageBody  } = req.body;
+    console.log("INSIDE SEND MESSAGE")
+    client.conversations.conversations(conversationSid)
+        .messages
+        .create({ body: messageBody})
+        .then(message => {
+            console.log(message.sid)
+            console.log("INSIDE SEND MESSAGE")
+        }).then(() => next()).catch(err => console.log("ERR", err))
+};
+
+const listAllMessagesWithParticpant = (req, res, next) => {
+    const { conversationSid  } = req.body;
+    client.conversations.conversations(conversationSid)
+      .messages
+      .list({limit: 20})
+      .then(messages => messages.forEach(m => console.log(m.body)))
+      .then(() => next()).catch(err => console.log("ERR", err))
+
+}
+
+module.exports = {
+    createConversation,
+    addParticipantToConversation,
+    sendMessageToParticipant,
+    listAllMessagesWithParticpant
+}
