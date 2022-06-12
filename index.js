f9668bfcca1aaf2bb2590d54a8cce6541e608e91
Louis Sheid <lxs3.dev@gmail.com>
2022-06-12 14:19:35 -0700

    add sockets and new-message route

--- index.js
+++ index.js
@@ -1,34 +1,68 @@
-const express = require('express')
-const app = express()
-const port = 8080
-require('dotenv').config();
-const bodyParser = require('body-parser')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 8080;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const { Server } = require("socket.io")
+const http = require('http');
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    // allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
 app.use(bodyParser.json());
 app.use(cors());
 
-const { createConversation, addParticipantToConversation, sendMessageToParticipant, listAllMessagesWithParticpant } = require("./middleware/conversations");
+const {
+  createConversation,
+  addParticipantToConversation,
+  sendMessageToParticipant,
+  listAllMessagesWithParticpant,
+} = require("./middleware/conversations");
 
-app.get('/', (req, res) => {
-  res.send('Arrow Plumbing Messaging API working!')
-})
-
-// twilioNumber, participantNumber
-app.post('/create-conversation', createConversation, addParticipantToConversation, (req,res) => {
-  const { conversationSid } = res.locals;
-  res.end(conversationSid)
+app.get("/", (req, res) => {
+  res.send("Arrow Plumbing Messaging API working!");
 });
 
+// twilioNumber, participantNumber
+app.post(
+  "/create-conversation",
+  createConversation,
+  addParticipantToConversation,
+  (req, res) => {
+    const { conversationSid } = res.locals;
+    res.end(conversationSid);
+  }
+);
+
 // converstaionId
-app.post('/send-message', sendMessageToParticipant, (req,res) => {
-  console.log("test")
+app.post("/send-message", sendMessageToParticipant, (req, res) => {
+  console.log("test");
   res.end("message has been sent");
 });
 
-app.post('/list-messages', listAllMessagesWithParticpant, (req, res) => {
+app.post("/list-messages", listAllMessagesWithParticpant, (req, res) => {
   res.end("test");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+app.post('/new-message', (req, res) => {
+  console.log('test')
+  io.emit({ test: "test"})
+});
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+server.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
